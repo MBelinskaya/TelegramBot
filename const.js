af97b0afc8899ebab453865c586cbdd90c553f6f
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-09-17 15:32:12 +0500

    add const.js

---
+++ const.js
@@ -0,0 +1,18 @@
+// Команды бота
+const commands = `
+/start - Перезапустить бота
+/reserve - Записаться на приёмку
+/help - Помощь
+`
+
+const content1 = `
+<b>Принять квартиру самостоятельно.</b> Чтобы принять квартиру самому, нужно всего лишь... <a href="https://youtube.com/">ссылка</a>
+`
+const content2 = `
+Записаться к профессионалу
+`
+
+
+module.exports.commands = commands
+module.exports.content1 = content1
+module.exports.content2 = content2
