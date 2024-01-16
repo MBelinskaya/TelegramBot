711336b758894de8d9730c17fcbeb3eb3d11e690
Mariya Belinskaya <mv.belinskaya@gmail.com>
2024-01-16 17:54:48 +0500

    add new menu items

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const { Telegraf, Markup } = require('telegraf')
-// const { message } = require('telegraf/filters')
+const { message } = require('telegraf/filters')
 require('dotenv').config()
+
 const content = require('./const')
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const buttonContentsStart = Markup.inlineKeyboard([[Markup.button.callback('📋 Принять квартиру самостоятельно', 'btn_1')],[Markup.button.callback('👷 Записаться на приёмку к специалисту', 'btn_2')]]);
 
 bot.help((ctx) => ctx.reply(content.commands))
 bot.start((ctx) => ctx.replyWithHTML(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'пользователь'}!
-<b>Выбери, что делать?</b>`, Markup.inlineKeyboard(
+<b>Выбери, что делать?</b>`, buttonContentsStart))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.on(message('text' && !'reserve'), (ctx) => ctx.reply('❓ Не понимаю тебя. Bыбери, что делать:', buttonContentsStart))
+bot.command('reserve', async (ctx) => {
+try {
+    await ctx.replyWithHTML('<b>Записаться на приёмку</b> квартиры, дома, ремота или тепловизионный осмотр', Markup.inlineKeyboard(
     [
-    [Markup.button.callback('Принять квартиру самостоятельно', 'btn_1')],
-    [Markup.button.callback('Записаться на приёмку к специалисту', 'btn_2')]
-    ]
-)))
-// bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+        [Markup.button.callback('📞 Телефон', 'btn_3'), Markup.button.callback('✍ WatsApp', 'btn_4')]
+        ]
+    ))
+    } catch (e) {
+        ctx.reply('Что-то пошло не так, используй команду /help')
+    }
+})
 
 // Button handling
 const addActionBot = (name, src, content) => {
@@ -23,15 +31,17 @@ const addActionBot = (name, src, content) => {
         if (src !== false) {
             await ctx.replyWithPhoto({source: src})
         }
-        await ctx.replyWithHTML(content, { disable_web_page_preview: true }) 
+        await ctx.replyWithHTML(content, { disable_web_page_preview: true }) // не будет изображения превью ссылки
     } catch (e) {
-        console.error(e)
+        ctx.reply('Что-то пошло не так, используй команду /help')
     }
     })
 }
 
-addActionBot('btn_1', false, content.content1)
-addActionBot('btn_2', './images/about.JPG', content.content2)
+addActionBot('btn_1', './images/about.JPG', content.content1)
+addActionBot('btn_2', false, content.content2)
+addActionBot('btn_3', false, content.content3)
+addActionBot('btn_4', false, content.content4)
 
 bot.launch()
 
